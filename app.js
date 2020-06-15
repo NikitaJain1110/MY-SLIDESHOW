720a362b65981196c17ccd55e73717e2499eaafb
NikitaJain1110 <56667079+NikitaJain1110@users.noreply.github.com>
2020-06-15 17:32:50 +0530

    second commit

--- app.js
+++ app.js
@@ -1,56 +1,61 @@
-const axios = require('axios')
-const http = require('http')
-const videoshow = require('videoshow')
-const file = require('file-api')
-const sharp = require('sharp')
+const axios     = require("axios");
+const http      = require("http");
+const videoshow = require("videoshow");
+const file      = require("file-api");
+
 const data = async () => {
-    try {
-      var data = await axios.get(
-        "https://jsonblob.com/api/jsonBlob/92283f4e-a23c-11ea-baec-594c4be2893f"
-      );
-      console.log(data.data.employees[0].background);
-      
-    await sharp(data.data.employees[2].background).resize({ height: 100, width: 100 }).toFile("Downloads/output_")
-    .then(function(newFileInfo) {
-        console.log("Success");
-    })
-    .catch(function(err) {
-        console.log("Error occured");
-    });
-      var aux = [
-        "1.jpg",
-        "2.jpg", 
-        "3.jpg"
-      ]
-     console.log(aux)
-     var videoOptions = {
-        loop: 9,
-        fps: 25,
-        transition: true,
-        transitionDuration: 1, // seconds
-        videoBitrate: 1024,
-        videoCodec: "libx264",
-        size: "64x?",
-        // audioBitrate: "128k",
-        // audioChannels: 2,
-        format: "mp4",
-        pixelFormat: "yuv420p",
-      };
-      var saveto = 'myslideshow.mp4'
-      await videoshow(aux, videoOptions)
-        .save(saveto)
-        .on("start", function (command) {
-          console.log("Conversion Started" + command);
-        })
-        .on("error", function (err, stdout, stderr) {
-          console.log(err);
-        })
-        .on("end", function (output) {
-          console.log("Conversion completed" + output);
-        });
+  try {
+    var data = await axios.get(
+      "https://jsonblob.com/api/jsonBlob/92283f4e-a23c-11ea-baec-594c4be2893f"
+    );
+
+    var aux = [
+      {"path": data.data.employees[1].background,"caption": data.data.employees[0].name +": "+ data.data.employees[0].description},
+      {"path": data.data.employees[1].background,"caption": data.data.employees[1].name +": "+ data.data.employees[1].description},
+      {"path": data.data.employees[1].background,"caption": data.data.employees[2].name +": "+ data.data.employees[2].description}
+    ];
+
+    var videoOptions = {
+      fps: 25,
+      loop: 5,
+      transition: true,
+      transitionDuration: 1,
+      videoBitrate: 1024,
+      videoCodec: "libx264",
+      size: "720x?",
+      format: "mp4",
+      subtitleStyles: {
+        Fontname: "Verdana",
+        Fontsize: "26",
+        PrimaryColour: "11861244",
+        SecondaryColour: "11861244",
+        TertiaryColour: "11861244",
+        BackColour: "-2147483640",
+        Bold: "2",
+        Italic: "0",
+        BorderStyle: "2",
+        Outline: "2",
+        Shadow: "3",
+        Alignment: "1",
+        MarginL: "40",
+        MarginR: "60",
+        MarginV: "40",
+      },
+    };
+
+    await videoshow(aux, videoOptions)
+      .save("myslideshow.mp4")
+      .on("start", function (command) {
+        console.log("Conversion Started" + command);
+      })
+      .on("error", function (err, stdout, stderr) {
+        console.log(err);
+      })
+      .on("end", function (output) {
+        console.log("Conversion completed:" + output);
+      });
     } catch (error) {
       console.error(error);
-    }
-  };
-  data();
-  console.log("done");
+  }
+};
+data();
